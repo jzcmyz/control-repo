9b212ac65d1470608bfdbfc7f3084785a00ebaf2
Gavin Ring <jzcmyz@gmail.com>
2016-04-03 23:36:35 +1000

    Update vmwaretoolsx module to install VMwareTools-9.0.0-782409.tar.gz

--- site/profile/manifests/vmwaretoolsx.pp
+++ site/profile/manifests/vmwaretoolsx.pp
@@ -1,8 +1,11 @@
 class profile::vmwaretoolsx {
 
+#
+# Installing .. VMwareTools-9.0.0-782409.tar.gz
+#
   class { 'vmwaretools':
     archive_url => 'http://apache-1.ring.net',
-    archive_md5 => '92672c721883b0d7a34123933ff2884e',
+    archive_md5 => '9df56c317ecf466f954d91f6c5ce8a6f',
   }
 
 }
