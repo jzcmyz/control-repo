715e5e4fb1f02725d9a830fc733272340f41c0ef
Gavin Ring <jzcmyz@gmail.com>
2016-04-04 23:17:48 +1000

    Added stahnma/epel module

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,6 +2,7 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
   include profile::collectd
+  include profile::epel
   include profile::ntp
   include profile::packages
 #  include profile::syslog
---
+++ site/profile/manifests/epel.pp
@@ -0,0 +1,6 @@
+class profile::epel {
+
+  include epel
+
+}
+
