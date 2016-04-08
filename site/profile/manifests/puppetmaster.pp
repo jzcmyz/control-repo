a4b8cfc830e371d178a48b0bc1c88adb287be555
Gavin Ring <jzcmyz@gmail.com>
2016-04-08 22:38:26 +1000

    Update repo_centos profile to use http://repo.ring.net

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -1,19 +1,17 @@
 class profile::puppetmaster {
 
 #
-# Open ports used by the Puppet console
+# Open ports used by Puppet
 #
   firewall { '020 open HTTP/HTTPS ports 80/433':
     proto => tcp,
     action => accept,
     dport => [ 80, 443 ],
   }
-  firewall { '817 open code manager port 8170':
+  firewall { '817 open Code Manager port 8170':
     proto => tcp,
     action => accept,
     dport => 8170,
   }
 
-
 }
-
--- site/profile/manifests/repo_centosx.pp
+++ site/profile/manifests/repo_centosx.pp
@@ -1,8 +1,8 @@
 class profile::repo_centosx {
 
   class {'repo_centos':
+    enable_mirrorlist => false,
     repourl => 'http://repo.ring.net',
   }
 
 }
-
