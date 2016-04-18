65c84ca2223a3e21c9971df6f675fac4e5e8fcaa
Gavin Ring <jzcmyz@gmail.com>
2016-04-18 22:44:30 +1000

    Updated role/plex

--- site/role/manifests/plex.pp
+++ site/role/manifests/plex.pp
@@ -1,4 +1,4 @@
-class role::downloader {
+class role::plex {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
