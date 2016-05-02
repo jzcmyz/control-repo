e7793a561062aac2b7af5351d58ff2698994c4db
Gavin Ring <jzcmyz@gmail.com>
2016-05-02 21:24:54 +1000

    Switch over to use WhatsARanjit-transmission

--- site/profile/manifests/transmissionx.pp
+++ site/profile/manifests/transmissionx.pp
@@ -45,15 +45,15 @@ class profile::transmissionx {
 #    ensure        => 'latest',
 #  }
 
-  class {'transmission::daemon':
-    rpc-authentication-required		=> true,
-    rpc-enabled		=> true,
-    trand	   => '/var/lib/transmission',
-    downloadr-_dir   => '/mnt/downloads',
-    rpc-username       => 'admin',
-    rpc-password   	=> '{b7518c56374798de52c8ef08d9eb1024119b7afeszwsK/7n',
-    rpc-port       	=> 9091,
-    rpc-whitelist  	=> ['127.0.0.1', '192.168.1.*'],
+  class {'transmission':
+    rpc_authentication_required         => true,
+    rpc_enabled         => true,
+    transd          => '/var/lib/transmission',
+    download_dir   => '/mnt/downloads',
+    rpc_username       => 'admin',
+    rpc_password        => '{b7518c56374798de52c8ef08d9eb1024119b7afeszwsK/7n',
+    rpc_port            => 9091,
+    rpc_whitelist       => ['127.0.0.1', '192.168.1.*'],
 #    blocklist_url  => 'http://list.iblocklist.com/?list=bt_templist&fileformat=p2p&archiveformat=gz',
   }
 
