664c860f35c5e9e043b14e62d6b133b5e2c73ce0
Gavin Ring <jzcmyz@gmail.com>
2016-06-28 20:41:26 +1000

    Add idm_master.pp module

---
+++ site/profile/manifests/idm_master.pp
@@ -0,0 +1,24 @@
+class profile::idm_master {
+
+  firewall { '020 open HTTP/HTTPS ports 80/433':
+    proto => tcp,
+    action => accept,
+    dport => [ 80, 443 ],
+  }
+  firewall { '021 open LDAP/LDAPS ports 389/636':
+    proto => tcp,
+    action => accept,
+    dport => [ 389, 636 ],
+  }
+  firewall { '022 open Kerberos ports 88/464':
+    proto => [ 'tcp', 'udp' ],
+    action => accept,
+    dport => [ 88, 464 ],
+  }
+  firewall { '023 open Dogtag Certificate System port 7389':
+    proto => tcp,
+    action => accept,
+    dport => 7389,
+  }
+
+}
