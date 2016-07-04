ea8cd8050192d4e02d127a9b645d5f78417e1926
Gavin Ring <jzcmyz@gmail.com>
2016-07-04 20:12:56 +1000

    Add dhcpd module

--- site/profile/manifests/dhcpd.pp
+++ site/profile/manifests/dhcpd.pp
@@ -7,24 +7,29 @@ class profile::dhcpd {
     dport => [ 67, 68 ],
   }
 
-  class {'dhcp':
+  class { 'dhcp':
+    service_ensure => stopped,
     dnsdomain    => [
       'ring.net',
       '1.168.192.in-addr.arpa',
       ],
     nameservers  => ['192.168.1.8'],
- #   interfaces   => ['eth0'],
  #   dnsupdatekey => "/etc/bind/keys.d/$ddnskeyname",
  #   require      => Bind::Key[ $ddnskeyname ],
-    pxeserver    => '192.168.1.6',
-    pxefilename  => 'bootstrap.ipxe',
+ #   pxeserver    => '192.168.1.6',
+ #   pxefilename  => 'pxelinux.0',
+    default_lease_time => 86400,
+    max_lease_time => 604800,
+
   }
 
   dhcp::pool{ 'ring.net':
-    network => '192.168.1.0',
+    network => '192.168.10',
     mask    => '255.255.255.0',
-    range   => '192.168.1.20 192.168.1.99',
+    range   => '192.168.1.100 192.168.1.140',
     gateway => '192.168.1.254',
   }
+
 }
+
 
