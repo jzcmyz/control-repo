23052ad8d0705405c26861c0b88c9fd14ace16bb
Gavin Ring <jzcmyz@gmail.com>
2016-08-07 22:22:59 +1000

    Enable syslog-ng module

--- site/profile/manifests/syslogx.pp
+++ site/profile/manifests/syslogx.pp
@@ -95,43 +95,43 @@ syslog_ng::filter {'f_news':
 #
 syslog_ng::log {'auth':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_auth' ]',
-  destinations  => [ 'd_auth' ]',
+  filters      => [ 'f_auth' ],
+  destinations => [ 'd_auth' ],
 }
 syslog_ng::log {'boot':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_boot' ]',
-  destinations => [ 'd_boot' ]',
+  filters      => [ 'f_boot' ],
+  destinations => [ 'd_boot' ],
 }
 syslog_ng::log {'cron':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_cron' ]',
-  destinations => [ 'd_cron' ]',
+  filters      => [ 'f_cron' ],
+  destinations => [ 'd_cron' ],
 }
 syslog_ng::log {'default':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_default' ]',
-  destinations => [ 'd_mesg' ]',
+  filters      => [ 'f_default' ],
+  destinations => [ 'd_mesg' ],
 }
 syslog_ng::log {'emergency':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_emergency' ]',
-  destinations => [ 'd_mlal' ]',
+  filters      => [ 'f_emergency' ],
+  destinations => [ 'd_mlal' ],
 }
 syslog_ng::log {'kernel':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_kernel' ]',
-  destinations => [ 'd_kern' ]',
+  filters      => [ 'f_kernel' ],
+  destinations => [ 'd_kern' ],
 }
 syslog_ng::log {'mail':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_mail' ]',
-  destinations => [ 'd_mail' ]',
+  filters      => [ 'f_mail' ],
+  destinations => [ 'd_mail' ],
 }
 syslog_ng::log {'news':
   sources      => [ 's_sys' ],
-  filters      => [ 'f_news' ]',
-  destinations => [ 'd_spol' ]',
+  filters      => [ 'f_news' ],
+  destinations => [ 'd_spol' ],
 }
 
 
@@ -160,15 +160,15 @@ syslog_ng::log {'news':
 # Definitions for the Syslog client
 #
 
-#  if $role == "client" {
-#    syslog_ng::destination {'d_logserver':
-#      definition => 'type(udp), host(logvault-1.ring.net),port(514);',
-#    }
-#    syslog_ng::log {'l_logserver':
-#    sources      => [ 's_sys' ],
-#    destinations => [ 'd_logserver' ]',
-#    }
-#  }
+  if $role == "client" {
+    syslog_ng::destination {'d_logserver':
+      definition => 'type(udp), host(logvault-1.ring.net),port(514);',
+    }
+    syslog_ng::log {'l_logserver':
+      sources      => [ 's_sys' ],
+      destinations => [ 'd_logserver' ],
+    }
+  }
 
 }
 
