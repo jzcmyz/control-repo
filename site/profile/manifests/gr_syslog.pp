86dc356fdd8cf61cf6c8446f76ee44cea280ad03
Gavin Ring <jzcmyz@gmail.com>
2016-08-30 22:41:42 +1000

    updates to the syslog manifest

--- site/profile/manifests/gr_syslog.pp
+++ site/profile/manifests/gr_syslog.pp
@@ -17,9 +17,9 @@ class profile::gr_syslog {
     package { 'syslog-ng-redis' :
       ensure => installed,
     }
-    file { '/etc/syslog-ng/conf.d/gr_syslog-ng_archiver.conf':
+    file { '/etc/syslog-ng/conf.d/gr_syslog_server.conf':
       ensure => file,
-      source => 'puppet:///modules/my_site/gr_syslog-ng_archiver.conf',
+      source => 'puppet:///modules/my_site/gr_syslog_server.conf',
       require => Package['syslog-ng'],
       notify  => Service['syslog-ng'],  # this sets up the relationship
     }
@@ -31,7 +31,7 @@ class profile::gr_syslog {
 
   file { '/etc/syslog-ng/syslog-ng.conf':
     ensure => file,
-    source => 'puppet:///modules/my_site/gr_syslog-ng.txt',
+    source => 'puppet:///modules/my_site/gr_syslog_client.conf',
     require => Package['syslog-ng'], 
     notify  => Service['syslog-ng'],  # Restart this service when this file changes
   } 
