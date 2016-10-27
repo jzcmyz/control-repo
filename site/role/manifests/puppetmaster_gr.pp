b31f9628dd6dc23a3fce77f5ce9a7957f5dca719
Gavin Ring <jzcmyz@gmail.com>
2016-10-27 15:07:45 +1100

    Created elastic_gr role

--- site/role/manifests/puppetmaster_gr.pp
+++ site/role/manifests/puppetmaster_gr.pp
@@ -1,4 +1,4 @@
-class role::puppet_master {
+class role::puppetmaster_gr {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
