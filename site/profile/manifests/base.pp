7dfdadec0a2d860dde691f77bb239108bb773383
Gavin Ring <jzcmyz@gmail.com>
2016-11-13 18:47:39 +1100

    Add vim for Puppetfile and remove repos from base.pp module

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,13 +2,13 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
   include profile::collectd
-  include profile::epel
+#  include profile::epel
   include my_fw
   include profile::networkx
   include profile::ntp
   include profile::packages
 #  include profile::postfixx
-  include profile::repo_centosx
+#  include profile::repo_centosx
   include profile::sshx
   include profile::syslog_gr
   include profile::vmwaretoolsx
