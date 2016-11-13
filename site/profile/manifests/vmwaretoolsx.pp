d96d873995f1e985f648f25801a2dd96971f3bf3
Gavin Ring <jzcmyz@gmail.com>
2016-11-13 22:13:20 +1100

    Updated vmwaretools module

--- site/profile/manifests/vmwaretoolsx.pp
+++ site/profile/manifests/vmwaretoolsx.pp
@@ -1,9 +1,9 @@
 class profile::vmwaretoolsx {
 
   class { 'vmwaretools':
-    version 	=> '10.0.6-3560309',
+    version 	=> '10.0.0-3000743',
     archive_url => 'http://apache-1.ring.net',
-    archive_md5 => '92672c721883b0d7a34123933ff2884e',
+    archive_md5 => '40f1c2c4077201877e02462454012888',
   }
 
 }
