592c2894e735edefbff6892b1b0b597ba8d573f6
Gavin Ring <jzcmyz@gmail.com>
2016-11-14 22:54:18 +1100

    Start using razorsedge vmwaretools module

--- site/profile/manifests/vmwaretoolsx.pp
+++ site/profile/manifests/vmwaretoolsx.pp
@@ -1,9 +1,9 @@
 class profile::vmwaretoolsx {
 
   class { 'vmwaretools':
-    version 	=> '10.0.0-3000743',
-    archive_url => 'http://apache-1.ring.net',
-    archive_md5 => '40f1c2c4077201877e02462454012888',
+ #   version 	=> '10.0.0-3000743',
+ #   archive_url => 'http://apache-1.ring.net',
+ #   archive_md5 => '40f1c2c4077201877e02462454012888',
   }
 
 }
