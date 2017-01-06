a1363e6d6ce0398f7501974383664ff1ea00274f
Gavin Ring <jzcmyz@gmail.com>
2017-01-06 16:40:14 +1100

    Add module to install windows packages

--- site/profile/manifests/windows_gr.pp
+++ site/profile/manifests/windows_gr.pp
@@ -7,6 +7,21 @@ class profile::windows_gr {
 #  notify{"here it is $packages":}
 #  notify{"here it is $redhat7packages":}
 
+  package { 'filezilla':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'gimp':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'launchy':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
   package { 'notepadplusplus':
     ensure   => installed,
     provider => chocolatey,
