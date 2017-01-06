f1a784761a014d6403e2ec6ac44bf9992216146a
Gavin Ring <jzcmyz@gmail.com>
2017-01-06 23:25:20 +1100

    Added WhatsARanjit/transmission module

--- site/profile/manifests/windows_gr.pp
+++ site/profile/manifests/windows_gr.pp
@@ -11,6 +11,11 @@ class profile::windows_gr {
     use_7zip                        => false,
   }
 
+  package { 'bginfo':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
   package { 'filezilla':
     ensure   => installed,
     provider => chocolatey,
@@ -21,12 +26,52 @@ class profile::windows_gr {
     provider => chocolatey,
   }
 
+  package { 'googlechrome':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'googledrive':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'keepass':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
   package { 'launchy':
     ensure   => installed,
     provider => chocolatey,
   }
 
   package { 'notepadplusplus':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'onedrive':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'putty':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'superputty':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'transgui':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+
+  package { 'vlc':
     ensure   => installed,
     provider => chocolatey,
   }
