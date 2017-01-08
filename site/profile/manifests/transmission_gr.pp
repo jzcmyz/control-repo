a94da295dc9d130dcd1e4d6e1171a2e09bd8fc3c
Gavin Ring <jzcmyz@gmail.com>
2017-01-08 23:59:33 +1100

    Updated rpc_whitelist_enabled parametre in transmission module

--- site/profile/manifests/transmission_gr.pp
+++ site/profile/manifests/transmission_gr.pp
@@ -36,7 +36,7 @@ class profile::transmission_gr {
     rpc_password                => '{5818041ff6f243971809f55e816a420b9beb6579cvOz.loY',
     rpc_port                    => 9091,
     rpc_whitelist_enabled       => true,
-    rpc_whitelist               => '127.0.0.1,192.168.1.*',
+    rpc_whitelist               => '192.168.1.*',
     transd			=> '/var/lib/transmission/.config/transmission-daemon',
   }
 
