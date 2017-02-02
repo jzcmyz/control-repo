0cf9ffb0f4f9a8abd57745f25278e752e1c2e816
Gavin Ring <jzcmyz@gmail.com>
2017-02-02 16:15:30 +1100

    Update windows package install list

--- site/profile/manifests/windows_gr.pp
+++ site/profile/manifests/windows_gr.pp
@@ -11,82 +11,82 @@ class profile::windows_gr {
     use_7zip => false,
   }
 
-  package { 'adobereader':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'adobereader':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'bginfo':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'bginfo':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'dotnet3.5':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'dotnet3.5':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'filezilla':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'filezilla':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'gimp':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'gimp':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'googlechrome':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'googlechrome':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'googledrive':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'googledrive':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'keepass':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'keepass':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'launchy':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'launchy':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'notepadplusplus':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'notepadplusplus':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'putty':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'putty':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'superputty':
-    ensure   => installed,
-    provider => chocolatey,
-    require  => Package['dotnet3.5']
-  }
+#  package { 'superputty':
+#    ensure   => installed,
+#    provider => chocolatey,
+#    require  => Package['dotnet3.5']
+#  }
 
-  package { 'transgui':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'transgui':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'vlc':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+#  package { 'vlc':
+#    ensure   => installed,
+#    provider => chocolatey,
+#  }
 
-  package { 'vmwarevsphereclient':
-    ensure   => installed,
-    provider => chocolatey,
-    require  => Package['dotnet3.5']
-  }
+#  package { 'vmwarevsphereclient':
+#    ensure   => installed,
+#    provider => chocolatey,
+#    require  => Package['dotnet3.5']
+#  }
 
 }
 
