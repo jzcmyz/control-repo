f20d4b67049b6335d95882b0ff2d036534d6bfc7
Gavin Ring <jzcmyz@gmail.com>
2017-04-15 21:38:05 +1000

    uninstall vmware tools packages

--- site/profile/manifests/vmwaretoolsx.pp
+++ site/profile/manifests/vmwaretoolsx.pp
@@ -1,6 +1,7 @@
 class profile::vmwaretoolsx {
 
   class { 'vmwaretools':
+    ensure => absent,
  #   version 	=> '10.0.0-3000743',
  #   archive_url => 'http://apache-1.ring.net',
  #   archive_md5 => '40f1c2c4077201877e02462454012888',
