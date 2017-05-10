dc8dd5692ca81d76fb33b6ba0c2d4698bf126a7b
Gavin Ring <jzcmyz@gmail.com>
2017-05-10 22:42:51 +1000

    Refacter the influxdb manifest

---
+++ site/profile/manifests/influxdb_gr.pp
@@ -0,0 +1,37 @@
+class profile::influxdb_gr {
+
+  firewall { '808 open influxdb ports 8083 8086':
+    proto => tcp,
+    action => accept,
+    dport => [ 8083, 8086 ],
+  }
+  
+  $global_config => {
+#    'bind-address'       => ':8088',
+    'reporting-disabled' => true,
+  }
+
+  $collectd_config = {
+    'default' => {
+      'enabled'          => true, # not default
+      'bind-address'     => ':25826',
+      'database'         => 'collectd',
+      'typesdb'          => '/usr/share/collectd/types.db',
+    }
+  }
+
+  $http_config = {
+    'enabled'              => true,
+    'auth-enabled'         => true,
+  }
+
+  class {'influxdb':
+    manage_repos    => true,
+    manage_service  => true,
+    global_config   => $global_config,
+    http_config     => $http_config,
+    collectd_config => $collectd_config,
+  }
+
+}
+
